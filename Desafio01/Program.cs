c6c45bd6629b3173738aa39c7f74f180e913ccfc
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2024-09-13 22:08:19 -0300

    feat: Implements code for Exercise 01

---
+++ Desafio01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.Write("Digite seu nome: ");
+    string nome = Console.ReadLine() ?? string.Empty;
+    if (!string.IsNullOrWhiteSpace(nome))
+    {
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+        break;
+    }
+
+    Console.WriteLine("Você deve digitar um nome!");
+}
