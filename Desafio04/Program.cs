f8b82f9684502f04d008c6a96e73e7e1c30c498a
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2024-09-13 23:11:43 -0300

    feat: Implements Exercise 05

---
+++ Desafio04/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.Write("Digite uma palavra (deixe em branco para sair): ");
+    var word = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(word)) break;
+
+    Console.WriteLine($"A palavra {word} tem {word.Length:N0} letras!");
+}
---
+++ Desafio05/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+string placa = CaptureText("Placa");
+
+string pattern = @"^[A-Za-z]{3}\d{4}$";
+string result = Regex.IsMatch(placa, pattern) && placa.Length == 7 ? "Verdadeiro" : "Falso";
+Console.WriteLine($"A placa {placa.ToUpperInvariant()} é valida: {result}");
+
+
+static string CaptureText(string prompt)
+{
+    string text = string.Empty;
+    while (true)
+    {
+        Console.Write($"{prompt.Replace(":", string.Empty).Trim()}: ");
+        text = Console.ReadLine() ?? string.Empty;
+        if (!string.IsNullOrEmpty(text)) break;
+        Console.WriteLine($"Você deve digitar seu {prompt.Replace(":", string.Empty).Trim()}.");
+    }
+
+    return text;
+}
