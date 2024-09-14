6a225b58db6778b5a2b7628e9f94e8da989ed0da
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2024-09-13 23:23:01 -0300

    feat: Implements Exercise 06

---
+++ Desafio06/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite uma Data: ");
+var textDate = Console.ReadLine();
+if (!DateTime.TryParse(textDate, out var date))
+{
+    Console.WriteLine("Você deve digitar uma data valida!");
+    return;
+}
+
+Console.WriteLine($"{date:F}");
+Console.WriteLine($"{date:dd/MM/yyyy}");
+Console.WriteLine($"{date:T}");
+Console.WriteLine($"{date:dd 'de' MMMM 'de' yyyy}");
+
