0244985dd047d64e48ca2c79c43ea0226314413d
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2024-09-13 22:22:59 -0300

    feat: Implements Exercise 02

---
+++ Desafio02/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+var nome = CaptureText("Nome:");
+var sobreNome = CaptureText("Sobrenome:");
+Console.WriteLine($"Seu nome completo é {nome} {sobreNome}");
+
+static string CaptureText(string prompt)
+{
+    string text = string.Empty;
+    while (true)
+    {
+        Console.Write($"{prompt.Replace(":", string.Empty).Trim()}: ");
+        text = Console.ReadLine() ?? string.Empty;
+        if (!string.IsNullOrEmpty(text)) break;
+        Console.WriteLine($"Você deve digitar seu {prompt.Replace(":", string.Empty).Trim()}.");
+    }
+
+    return text;
+}
